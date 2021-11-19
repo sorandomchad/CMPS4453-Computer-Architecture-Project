6cc09724077b66181a23957d70d8025a0dbe570d
sorandomchad <chaddon1999@gmail.com>
2021-11-19 09:46:40 -0600

    add code to retrieve tag and offset from address

--- project.cpp
+++ project.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<fstream>
+#include<bitset>
 
 using namespace std;
 
@@ -7,6 +8,7 @@ struct Set
 {
     unsigned int line1;
     unsigned int line2;
+    int referenced;
 
 };
 
@@ -14,19 +16,28 @@ int main()
 {
     ifstream f("test.txt");
     struct Set cache[32];
-    unsigned int vCache[2] = {-1, -1};
+    unsigned int vCache[2] = {0, 0};
     int misses = 0;
-    unsigned int code, address, set, tag;
+    unsigned int code, address, set, tag, tag_set;
 
     for(int i = 0; i < 32; i++)
     {
-        cache[i].line1 = -1;
-        cache[i].line2 = -1;
+        cache[i].line1 = 0;
+        cache[i].line2 = 0;
+        cache[i].referenced = -1;
     }
 
     while(f >> code >> hex >> address)
     {
-        cout << hex << address << endl;
+        tag_set = address >> 6;
+        tag = address >> 11;
+        set = tag_set & 31;
+        
+        // bitset<32> t(set);
+        // cout << t << endl;
+
+        cout << tag << " ";
+        cout << set << endl;
     }
 
     f.close();
